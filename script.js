299c9850313659b025360113baf3cd94c6ee3dec
hhormax <hormax14@gmail.com>
2024-09-15 00:53:02 +0300

    fix +mraid

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 import { MatchThree } from "./matchThree.js";
 
+let gameInstance;
+
 function startGame() {
-  new MatchThree(8, 8, 6);
+  const wrap = document.querySelector(".wrap");
+  wrap.innerHTML = "";
+  gameInstance = new MatchThree(8, 8, 6);
 }
 
-startGame();
+function restartGame() {
+  document.querySelector(".score").innerHTML = "0";
+  startGame();
+}
+
+function initGame() {
+  startGame();
+}
+
+function onMraidReady() {
+  if (mraid.getState() === "loading") {
+    mraid.addEventListener("stateChange", (state) => {
+      if (state === "default") {
+        initGame();
+      }
+    });
+  } else {
+    initGame();
+  }
+}
+
+function setupMraid() {
+  if (typeof mraid !== "undefined") {
+    mraid.addEventListener("ready", onMraidReady);
+    mraid.addEventListener("viewableChange", checkVisibility);
+  } else {
+    initGame();
+  }
+}
+
+document.querySelector(".restart").addEventListener("click", restartGame);
+setupMraid();
